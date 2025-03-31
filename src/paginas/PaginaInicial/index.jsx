41a73ffef3cf2b9e0155db7c45f1e17bfcbed5b1
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2025-03-31 00:33:07 -0300

    Curso de React com JavaScript: lidando com arquivos estáticos(finalizando a parte 'Fontes e Estilos'

---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,16 @@
+import Banner from "../../componentes/Banner";
+import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import styles from "./PaginaInicial.module.scss";
+
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <section className={styles.principal}>
+        <Menu />
+        <Banner />
+      </section>
+    </>
+  );
+}
